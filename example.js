4d16fdf25b0d7af3030ca379b084007d0290fb15
mapster <alexander@rosbach.no>
2016-06-06 19:18:12 +0200

    Some cosmetics

--- example.js
+++ example.js
@@ -4,13 +4,15 @@
     read data from a TI sensorTag.
     The sensortag library functions are all asynchronous and there is a
     sequence that must be followed to connect and enable sensors.
-      To connect:
+      Step 1: Connect
         1) discover the tag
         2) connect to and set up the tag
-      To activate sensor:
+      Step 2: Activate sensors
         3) turn on the sensor you want to use (in this case, IR temp)
         4) turn on notifications for the sensor
+      Step 3: Register listeners
         5) listen for changes from the sensortag
+      Step 4 (optional): Configure sensor update interval
 */
 var SensorTag = require('sensortag');
 
@@ -20,13 +22,23 @@ var log = function(text) {
   }
 }
 
-// Connect to sensortag device. It's address is printed on the inside of the red sleeve (replace the one below).
-var connected = new Promise((resolve, reject) => SensorTag.discoverByAddress("b0:b4:48:c3:5a:03", (tag) => resolve(tag)))
+//==============================================================================
+// Step 1: Connect to sensortag device.
+//------------------------------------------------------------------------------
+// It's address is printed on the inside of the red sleeve
+// (replace the one below).
+var ADDRESS = "b0:b4:48:c3:5a:03";
+var connected = new Promise((resolve, reject) => SensorTag.discoverByAddress(ADDRESS, (tag) => resolve(tag)))
   .then((tag) => new Promise((resolve, reject) => tag.connectAndSetup(() => resolve(tag))));
 
-// Enable the sensors you need. For a list of available sensors, and other functions, see https://github.com/sandeepmistry/node-sensortag.
+//==============================================================================
+// Step 2: Enable the sensors you need.
+//------------------------------------------------------------------------------
+// For a list of available sensors, and other functions,
+// see https://github.com/sandeepmistry/node-sensortag.
 // For each sensor enable it and activate notifications.
-// Remember that the tag object must be returned to be able to call then on the sensor and register listeners.
+// Remember that the tag object must be returned to be able to call then on the
+// sensor and register listeners.
 var sensor = connected.then(function(tag) {
   log("connected");
 
@@ -37,12 +49,13 @@ var sensor = connected.then(function(tag) {
   tag.notifyHumidity(log);
 
   tag.enableGyroscope(log);
-  tag.notifyHumidity(log);
+  tag.notifyGyroscope(log);
   return tag;
 });
 
+//==============================================================================
+// Step 3: Register listeners on the sensor.
 //------------------------------------------------------------------------------
-// Register listeners on the sensor.
 // You can register multiple listeners per sensor.
 //
 
@@ -66,9 +79,10 @@ sensor.then(function(tag) {
   })
 });
 
+//==============================================================================
+// Step 4 (optional): Configure periods for sensor reads.
 //------------------------------------------------------------------------------
-// Configure periods for sensor reads. The registered listeners will be invoked
-// with the specified interval.
+// The registered listeners will be invoked with the specified interval.
 sensor.then(function(tag) {
   tag.setIrTemperaturePeriod(3000, log);
 });

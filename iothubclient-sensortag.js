26dad7f610f77291def7d24e80e95e65cb80a093
mapster <alexander@rosbach.no>
2016-06-19 16:16:57 +0200

    Change iothub-client to initiate sensor:data events

--- iothubclient-sensortag.js
+++ iothubclient-sensortag.js
@@ -1,5 +1,13 @@
-var SENSORTAG_ADDRESS = "b0:b4:48:c3:5a:03";
 var HUB_ADDRESS = process.argv[2] || 'http://localhost:3000';
+var ID = process.argv[3] || 'sensortag1';
+var SENSORTAG_ADDRESS = process.argv[4] || 'b0:b4:48:c3:5a:03';
+
+/**
+ * A simple IOT-Hub-client example for the sensortag device.
+ * It sets up logging of irTemperature every second.
+ *
+ * See https://github.com/mapster/iothub for an IOT-Hub to connect to. 
+ */
 
 var socket = require('socket.io-client')(HUB_ADDRESS);
 var SensorTag = require('sensortag');
@@ -10,9 +18,14 @@ var log = function(text) {
   }
 }
 
-var _sensorData = {
-  temp: []
-};
+var payload = function(sensor, data) {
+  return {
+    id: ID,
+    type: 'sensortag',
+    sensor: sensor,
+    data: data
+  };
+}
 
 var connected = new Promise((resolve, reject) => SensorTag.discoverByAddress(SENSORTAG_ADDRESS, (tag) => resolve(tag)))
   .then((tag) => new Promise((resolve, reject) => tag.connectAndSetup(() => resolve(tag), log)));
@@ -21,23 +34,17 @@ var sensor = connected.then(function(tag) {
   log('SensorTag: connected to ' + SENSORTAG_ADDRESS);
   tag.enableIrTemperature(log);
   tag.notifyIrTemperature(log);
+  tag.setIrTemperaturePeriod(1000, log);
+
   return tag;
 });
 
 sensor.then(function(tag) {
   tag.on('irTemperatureChange', function(objectTemp, ambientTemp) {
-    _sensorData.temp.push({objectTemp, ambientTemp});
+    socket.emit('sensor:data', payload('irTemperature', {objectTemp, ambientTemp}));
   });
 });
 
 socket.on('connect', function () {
-    console.log('SensorTag logger: connected to hub ' + HUB_ADDRESS);
-});
-
-socket.on('logger:history', function () {
-    socket.emit('server:results', _sensorData.temp);
-});
-
-socket.on('logger:read', function () {
-  socket.emit('server:results', [_sensorData.temp[_sensorData.temp.length - 1]]);
+      log('SensorTag logger: connected to IOT-Hub at ' + HUB_ADDRESS);
 });
